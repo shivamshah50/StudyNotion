b4a5cbc8bbafdde3cbbc00640bc0cfdf324720a0
shivamshah50 <shivamshah781@gmail.com>
2025-05-12 20:58:22 +0530

    code working

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,5 +1,5 @@
-const BASE_URL = "https://study-notion-58uaz18f7-shivamshah12s-projects.vercel.app/api/v1"
-
+// const BASE_URL = "https://study-notion-58uaz18f7-shivamshah12s-projects.vercel.app/api/v1"
+const BASE_URL = "http://localhost:4000/api/v1";
 // AUTH ENDPOINTS
 export const endpoints = {
   SENDOTP_API: BASE_URL + "/auth/sendotp",
