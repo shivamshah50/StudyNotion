f9fd7dd18688d1e90f55604e3e4a69dcf7ecfd03
shivamshah50 <shivamshah781@gmail.com>
2025-05-12 21:44:55 +0530

    backend added

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://study-notion-58uaz18f7-shivamshah12s-projects.vercel.app/api/v1"
+const BASE_URL = "https://studynotion-3-cjjr.onrender.com/api/v1"
 // const BASE_URL = "http://localhost:4000/api/v1";
 // AUTH ENDPOINTS
 export const endpoints = {
